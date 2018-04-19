c722bf02ced4d0be1fa5eb630d126ed59bf61e4c
yoshi11111 <30550142+yoshi11111@users.noreply.github.com>
2018-04-19 22:23:24 +0900

    TEST
    TEST

--- excelAll/Form1.cs
+++ excelAll/Form1.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,10 +17,43 @@ namespace エクセルテキスト全般検索
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("aa");
+            D d = SH;
+            await Task.Run(() => { KK(d); });
+
+        }
+
+        public delegate void D();
+
+
+        public void SH()
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+
+
+        public void KK(D a)
+        {
+            List<Task> taskList = new List<Task>();
+            for (int i = 0; i < 10; i++)
+            {
+                Task t = Task.Factory.StartNew(() => { asy(); });
+                taskList.Add(t);
+            }
+            foreach (Task t in taskList)
+            {
+                t.Wait();
+
+            }
+
+            this.BeginInvoke(a, null);
+        }
+
+        public void asy()
+        {
+            Thread.Sleep(10000);
+
         }
     }
 }
