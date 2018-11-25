9e035eeb261d1055a1988eb8f964e2ad3d961737
yoshi11111 <30550142+yoshi11111@users.noreply.github.com>
2018-11-25 17:08:41 +0900

    test
    test

---
+++ CodeAssist/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace CodeAssist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.rtbCode = new System.Windows.Forms.RichTextBox();
+            this.btnFech = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.rtbCode, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnFech, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnSave, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.99437F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.005629F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(530, 498);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // rtbCode
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.rtbCode, 3);
+            this.rtbCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbCode.Location = new System.Drawing.Point(3, 3);
+            this.rtbCode.Name = "rtbCode";
+            this.rtbCode.Size = new System.Drawing.Size(524, 447);
+            this.rtbCode.TabIndex = 0;
+            this.rtbCode.Text = "";
+            // 
+            // btnFech
+            // 
+            this.btnFech.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnFech.Location = new System.Drawing.Point(3, 456);
+            this.btnFech.Name = "btnFech";
+            this.btnFech.Size = new System.Drawing.Size(324, 39);
+            this.btnFech.TabIndex = 1;
+            this.btnFech.Text = "変数名関数名解析";
+            this.btnFech.UseVisualStyleBackColor = true;
+            this.btnFech.Click += new System.EventHandler(this.btnFech_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSave.Location = new System.Drawing.Point(333, 456);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 39);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "ローカル保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(433, 456);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "ローカル読込";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(544, 533);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(536, 504);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tableLayoutPanel2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(536, 504);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.btnSearch, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tbSearch, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.listView1, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.groupBox1, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(530, 498);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSearch.Location = new System.Drawing.Point(433, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(94, 24);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "検索";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbSearch.Location = new System.Drawing.Point(3, 3);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(424, 22);
+            this.tbSearch.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.tableLayoutPanel2.SetColumnSpan(this.listView1, 2);
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("MS UI Gothic", 11F);
+            this.listView1.Location = new System.Drawing.Point(3, 63);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(524, 432);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.tableLayoutPanel2.SetColumnSpan(this.groupBox1, 2);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(524, 24);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 5);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "変数名一覧";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(124, 5);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "関数名一覧";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 533);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox rtbCode;
+        private System.Windows.Forms.Button btnFech;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ CodeAssist/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeAssist
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ////列の追加
+            listView1.Columns.Add(
+                "", 100, HorizontalAlignment.Left);
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            listView1.HeaderStyle = ColumnHeaderStyle.None;
+
+
+        }
+
+        // 変数名
+        public static List<string> vList = new List<string>();
+
+        // 関数名
+        public static List<string> fList = new List<string>();
+
+
+        // コメント
+        public static List<string> cList = new List<string>();
+
+
+        private void btnFech_Click(object sender, EventArgs e)
+        {
+
+            string code = rtbCode.Text;
+
+            AddVariableList(code, ref vList);
+
+            AddFNameList(code, ref fList);
+
+
+        }
+
+        private void AddVariableList(string code, ref List<string> vList)
+        {
+
+            // 前後が半角スペースで、かつ途中にドットもかっこもない
+            System.Text.RegularExpressions.Regex r =
+                new System.Text.RegularExpressions.Regex(
+                     @" [\w-]+ ",
+                        System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            //TextBox1.Text内で正規表現と一致する対象を1つ検索
+            System.Text.RegularExpressions.Match m = r.Match(code);
+
+            while (m.Success)
+            {
+                string value = m.Value.Trim();
+
+                if (!string.IsNullOrEmpty(value) && !vList.Contains(value))
+                {
+                    //一致した対象が見つかったときキャプチャした部分文字列を表示
+                    vList.Add(value);
+                }
+
+                //次に一致する対象を検索
+                m = m.NextMatch();
+
+            }
+
+        }
+
+
+        private void AddFNameList(string code, ref List<string> fList)
+        {
+
+            // 前後が半角スペースで、かつ途中にドットもかっこもない
+            System.Text.RegularExpressions.Regex r =
+                new System.Text.RegularExpressions.Regex(
+                     @" [\w-]+\(",
+                        System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            //TextBox1.Text内で正規表現と一致する対象を1つ検索
+            System.Text.RegularExpressions.Match m = r.Match(code);
+
+            while (m.Success)
+            {
+                string value = m.Value.Trim();
+
+                if (!string.IsNullOrEmpty(value) && !fList.Contains(value))
+                {
+                    //一致した対象が見つかったときキャプチャした部分文字列を表示
+                    fList.Add(value);
+
+                }
+
+                //次に一致する対象を検索
+                m = m.NextMatch();
+
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Util.Write();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Util.Read();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string word = tbSearch.Text.Trim();
+            if (string.IsNullOrEmpty(word))
+            {
+                return;
+            }
+
+            List<string> tmpVList = (from v in vList
+                                     where (v.StartsWith(word.ToUpper()) || v.StartsWith(word.ToLower()))
+                                     select v).ToList();
+            List<string> restVList = (from v in vList
+                                      where !(v.StartsWith(word.ToUpper()) || v.StartsWith(word.ToLower()))
+                                      select v).ToList();
+            vList = tmpVList;
+            vList.AddRange(restVList);
+
+
+            List<string> tmpFList = (from v in fList
+                                     where (v.StartsWith(word.ToUpper()) || v.StartsWith(word.ToLower()))
+                                     select v).ToList();
+            List<string> restFList = (from v in fList
+                                      where !(v.StartsWith(word.ToUpper()) || v.StartsWith(word.ToLower()))
+                                      select v).ToList();
+
+
+            fList = tmpFList;
+            fList.AddRange(restFList);
+
+            ShowList();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            ShowList();
+        }
+
+
+        public void ShowList()
+        {
+            List<string> tmplist;
+            if (radioButton1.Checked)
+            {
+                tmplist = vList;
+            }
+            else
+            {
+                tmplist = fList;
+
+            }
+            listView1.Items.Clear();
+            foreach (string value in tmplist)
+            {
+                ListViewItem item = new ListViewItem(value);
+                listView1.Items.Add(item);
+
+            }
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
