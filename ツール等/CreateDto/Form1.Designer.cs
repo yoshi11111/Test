cd6037c9a08c6a54aeceb1e91cd2fa5ccdbe9e15
yoshi11111 <30550142+yoshi11111@users.noreply.github.com>
2020-06-29 20:22:10 +0900

    dto

---
+++ ツール等/CreateDto/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace CreateDto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(481, 486);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(475, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(475, 370);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(475, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Dtoクラス作成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "テーブル物理名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "[物理名,データ型](カンマ区切り）";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 486);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ツール等/CreateDto/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace CreateDto
+{
+    public partial class Form1 : Form
+    {
+        public class Row
+        {
+            public string ColNameSnake { get; set; }
+            public string ColNamePascal { get; set; }
+            public string Type { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tableName = ToPascal(textBox1.Text.ToLower());
+            List<Row> List = new List<Row>();
+            using (System.IO.StringReader rs = new System.IO.StringReader(richTextBox1.Text))
+            {
+                while (rs.Peek() > -1)
+                {
+                    Row row = new Row();
+                    if (!IsValid(rs.ReadLine(), ref row))
+                    {
+                        continue;
+                    }
+                    List.Add(row);
+                }
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"public class {tableName}");
+            sb.AppendLine("{");
+            foreach(Row row in List)
+            {
+                sb.AppendLine($"[DataColumn(name=\"{row.ColNameSnake}\")]");
+                sb.AppendLine($"public {row.Type} {row.ColNamePascal}"+"{get;set;}");
+            }
+            sb.AppendLine("}");
+            richTextBox1.Text = sb.ToString();
+        }
+
+        private bool IsValid(string line, ref Row row)
+        {
+            line = line.Replace(" ", "");
+            line = line.Replace("　", "");
+            string[] vals = line.Split(',');
+            try
+            {
+                if (vals.Count() != 2)
+                {
+                    return false;
+                }
+                foreach (string val in vals)
+                {
+                    if (string.IsNullOrEmpty(val) || val.Contains("\"") || val.Contains("\'"))
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            row.ColNameSnake = vals[0];
+            row.ColNamePascal = ToPascal(vals[0].ToLower());
+            row.Type = ConvertType(vals[1]);
+            return true;
+        }
+
+
+        public string ConvertType(string type)
+        {
+            type = type.Replace("NVARCHAR", "string");
+            type = type.Replace("DATETIME", "DateTime?");
+            type = type.Replace("DECIMAL", "decimal?");
+            return type;
+        }
+
+
+        private static string ToPascal(string text)
+        {
+            return Regex.Replace(
+                text.Replace("_", " "),
+                @"\b[a-z]",
+                match => match.Value.ToUpper()).Replace(" ", "");
+        }
+    }
+
+}
