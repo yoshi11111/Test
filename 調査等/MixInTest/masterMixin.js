a106a6bf2eeaed7eaa63ebc6a6bf41ab6af44717
yoshi11111 <30550142+yoshi11111@users.noreply.github.com>
2020-06-30 21:31:14 +0900

    MixinTest

---
+++ 調査等/MixInTest/masterMixin.js
@@ -0,0 +1,29 @@
+﻿var current1 = this;
+var masterMixin = {
+    data: {
+        dataObj: {}
+    },
+    methods: {
+        doPost: function (postType) {
+            // ここが重要
+            var current2 = this;
+            const options = {
+                headers: { 'Content-Type': 'application/json; charset=utf-8' }
+            };
+            param = { json: JSON.stringify(this.dataObj), type: postType };
+            axios.post(URL + '/DoPost', param, options)
+                .then(function (response) {
+                    // OK
+                    current2.dataObj = JSON.parse(response.data.d);
+                    // NG  current1.dataObj = JSON.parse(response.data.d);
+                    // NG     this.dataObj = JSON.parse(response.data.d);
+                    // NG     masterMixin.dataObj = JSON.parse(response.data.d);
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+};
+
